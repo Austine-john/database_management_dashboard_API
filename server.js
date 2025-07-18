be6075213a5b946e0206626e1e791db0a7de31cc
Austine-john <ochiengaustine582@gmail.com>
2025-07-18 23:44:17 +0300

    CORS updates

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// Adding CORS headers manually
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // allow any domain
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
